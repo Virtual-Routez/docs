6157aa8943da6ba5097423dc153c8b0035f16318
Dani <dani.simeonov@stratusolve.com>
2021-06-08 09:27:01 +0200

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Virtual Routez',
+  tagline: 'Track your supply chain from start to finish',
+  url: 'https://JohanGriesel.github.io',
+  baseUrl: '/virtual-routez-docs/',
+  organizationName: 'JohanGriesel', // Usually your GitHub org/user name.
+  projectName: 'virtual-routez-docs', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  themeConfig: {
+    navbar: {
+      title: 'Virtual Routez',
+      logo: {
+        alt: 'VRZ Logo',
+        src: 'img/app_logo.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        // {to: '/blog', label: 'Blog', position: 'left'}, // No blog
+        {
+          href: 'https://github.com/johangriesel/virtual-routez-docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            // {
+            //   label: 'Integration',
+            //   to: '/docs/integration',
+            // },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Virtual Routez. Built with Divblox.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/johangriesel/virtual-routez-docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
