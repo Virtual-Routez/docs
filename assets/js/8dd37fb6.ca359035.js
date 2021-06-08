d7e68e83e5735f426492365057642002c359ba4e
Dani <dani.simeonov@stratusolve.com>
2021-06-08 11:09:18 +0200

    Re-init docusaurus project

--- assets/js/8dd37fb6.ca359035.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkvirtual_routez_docs=self.webpackChunkvirtual_routez_docs||[]).push([[201],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
